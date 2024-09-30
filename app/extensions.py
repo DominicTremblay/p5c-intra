b87f8d644d95178898488b428c4dc924b2b80e57
Dominic Tremblay <dominictremblay24@gmail.com>
2024-09-30 11:57:31 -0400

    ajouter la config seeder dans extensions

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_seeder import FlaskSeeder
 
 # Initialize the extensions here
-db = SQLAlchemy()
-migrate = Migrate()
+db = SQLAlchemy()      # Initialize SQLAlchemy
+migrate = Migrate()    # Initialize Flask-Migrate
+seeder = FlaskSeeder() # Initialize Flask-Seeder (but to be connected to app later)
