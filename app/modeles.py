aab2eb65ac517d4e87716c245e0c73d2a93357c0
Dominic Tremblay <dominictremblay24@gmail.com>
2024-10-03 16:42:50 -0400

    ajout modele

--- app/modeles.py
+++ app/modeles.py
@@ -1,1 +1,7 @@
 from app.extensions import db
+
+class Film(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  titre = db.Column(db.String(100), nullable=False)
+  genre = db.Column(db.String(50))
+  annee_sortie = db.Column(db.Integer)
